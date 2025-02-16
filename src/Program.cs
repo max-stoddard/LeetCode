4da0acd15bf39a1fb3ce45cf23bf7b468f9dfa25
Max Intellij Laptop <ms4623@ic.ac.uk>
2025-02-16 21:56:50 +0000

    feat: 128

--- src/Program.cs
+++ src/Program.cs
@@ -2,31 +2,42 @@
 
 namespace LeetCode
 {
-  public class Solution
-  {
-    public static void Main(string[] args)
+    public class Solution
     {
-      Console.WriteLine("Hello from LeetCode!");
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from LeetCode!");
+        }
+
+        public int LongestConsecutive(int[] nums)
+        {
+            // Turn nums to hashSet
+
+            // Find a start element
+
+            // See how deep it goes
+
+            // Return best of start elements
+
+            HashSet<int> set = new HashSet<int>(nums);
+            int best = 0;
+
+            foreach (int n in set)
+            {
+                if (!set.Contains(n - 1))
+                {
+                    // n is first element in sequenct
+                    int cnt = 1;
+                    while (set.Contains(n + cnt))
+                    {
+                        cnt++;
+                    }
+
+                    best = Math.Max(cnt, best);
+                }
+            }
+
+            return best;
+        }
     }
-
-    public static int LongestConsecutive(int[] nums) {
-        // n + 1
-        // HashMap n + 1 => no in sequence already
-        // n => (n + 1, 1)
-        // n + 1 => (n + 2, 2)
-        // n, n + 2 => (n + 1, 1), (n + 3, 1)
-        //      n + 1 => (n + 2, 2), (n + 3, 1) => check if n + 2 + 1 on increment
-        int n = nums.Length;
-        if (n <= 1)
-            return n;
-
-        Dictionary<int, int> map = [];
-
-        // Process
-
-        return map
-            .Select(kvp => kvp.)
-            .Max();
-    }
-  }
 }
